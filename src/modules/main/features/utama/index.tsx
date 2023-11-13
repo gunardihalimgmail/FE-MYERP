ac62d9bf0c53b08cf0f492a26c613f91c97c4ac7
gunardi <gunardihalim@gmail.com>
2023-11-13 17:52:59 +0700

    font and image

--- src/modules/main/features/utama/index.tsx
+++ src/modules/main/features/utama/index.tsx
@@ -13,11 +13,12 @@ const MainPage = () => {
           <div className='sub-wrapper'>
 
             <div className='side-left'>
-                <div>
+                <div className='side-left-sub'>
+                  
                     <Sidebar
                       className='sidebarClasses'
                       backgroundColor='rgb(249,249,249,0.5)'
-                      collapsed={false}
+                      collapsed={false} // menu pindah ke kanan
                     >
                         <Menu
                           menuItemStyles={{
@@ -27,21 +28,24 @@ const MainPage = () => {
                                   return {
                                       // color: disabled ? '#f5d9ff' : '#d359ff',
                                       backgroundColor: active ? '#eecef9' : undefined,
-                                      fontWeight: 600,
+                                      fontWeight: 500,
                                       paddingLeft:'25px',
                                       ['&:hover']:{
                                         backgroundColor:'rgb(197, 228, 255)',
                                         color:'rgb(68, 89, 110)',
                                         borderRadius:'0'
                                       },
-                                      ['&.active']: {
+                                      ['&.active']: { 
                                         backgroundColor: '#13395e',
                                         color: '#b6c8d9',
                                       },
                                       // ketika menu item level 0 lagi open
                                       ['&.ps-open']:{
-                                        fontWeight: 'bold' 
+                                        fontFamily:'Titillium_Web_Bold'
                                       }
+                                      ,
+                                      fontFamily:'Titillium_Web_Regular',
+                                      fontSize:'14px'
                                   }
                               }
                               else
@@ -79,26 +83,49 @@ const MainPage = () => {
                               // }
                           }}
                         >
-                          <div className='pre-title-submenu'>
-                            <p>General</p>
+
+                          <div className='logo-header'>
+                              <div className='logo-title'>
+                                  P
+                              </div>
+                              <p className='logo-subtitle'>
+                                Pro Dash
+                              </p>
                           </div>
 
-                            <SubMenu label="Charts" defaultOpen={true} icon={svgCustom('searchengin', 'rgb(0, 152, 229)', 18)}>
+                          <div className='pre-title-submenu'>
+                              <p>General</p>
+                          </div>
+
+                            <SubMenu className='submenu-custom' label="Charts" defaultOpen={false} icon={svgCustom('searchengin', 'rgb(7, 110, 97)', 18)}>
                                 <MenuItem>Pie Charts</MenuItem>
                                 <MenuItem>Line Charts</MenuItem>
                             </SubMenu>
-                            <MenuItem icon={svgCustom('rectangle-list', 'rgb(0, 152, 229)', 18)}>Dashboard</MenuItem>
-                            <SubMenu label="Transaksi" defaultOpen ={true} icon={svgCustom('file-lines', 'rgb(0, 152, 229)', 18)}>
+                            <MenuItem className='submenu-custom' icon={svgCustom('rectangle-list', 'rgb(7, 110, 97)', 18)}>Dashboard</MenuItem>
+                            <SubMenu label="Transaksi" defaultOpen ={false} icon={svgCustom('file-lines', 'rgb(7, 110, 97)', 18)}>
                                 <MenuItem>Sales</MenuItem>
                                 <MenuItem>Purchase</MenuItem>
                             </SubMenu>
+
+                            <SubMenu className='submenu-custom' label="Charts" defaultOpen={false} icon={svgCustom('searchengin', 'rgb(7, 110, 97)', 18)}>
+                                <MenuItem>Pie Charts</MenuItem>
+                                <MenuItem>Line Charts</MenuItem>
+                            </SubMenu>
+                            <MenuItem className='submenu-custom' icon={svgCustom('rectangle-list', 'rgb(7, 110, 97)', 18)}>Dashboard</MenuItem>
+
                         </Menu>
                     </Sidebar>
+                        <div className='side-bottom'>
+                            <div className='logo-img-bottom'>
+                            </div>
+                        </div>
                 </div>
             </div>
             <div className='side-right'>
                 <HeaderMain></HeaderMain>
-                <div style = {{height:'300%', padding: '2rem'}}>body</div>
+                <div style = {{height:'300%', padding: '2rem'}}>
+                    {/* Body Content */}
+                </div>
             </div>
 
           </div>
